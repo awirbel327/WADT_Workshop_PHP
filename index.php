36049ba8437aa99381094a7aabbb41c908b1498c
awirbel327 <67149628+awirbel327@users.noreply.github.com>
2022-10-09 15:00:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link rel="stylesheet" href="style.php" media="screen"> for the next time-->
+    <link rel="stylesheet" href="php.css">
+    <title>PHP Homework</title>
+</head>
 
+<body>
+    <?php
+        function dateAndTime(){
+          
+            echo "<br>The date is: " .date('d-m-Y'); 
+            echo "<br>The time is: " . date("h:i:sa");
+        }
+    ?>
+    <br><br><br>
+
+    <form  action="returnData.php" method="post">
+    Surename: <input type="text" name="surename"><br><br>
+    Name: <input type="text" name="name"><br><br>
+    Birthday: <input type="text" name="bday"><br><br>
+    <input type="submit">
+    </form>
+    <br><br>
+    
+    <footer class="footer bg-black small text-center text-white-50">
+        <div class="container px-4 px-lg-5">Copyright &copy; Unicorn GmbH & Co. kg</div>
+        <?php
+        dateAndTime();
+        ?>
+    </footer>
+</body>
+</html>
