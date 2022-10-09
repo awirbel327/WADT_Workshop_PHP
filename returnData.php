2d2dfb091766b4ac0cad9c19f14f0ad47687862e
awirbel327 <67149628+awirbel327@users.noreply.github.com>
2022-10-09 15:03:04 +0200

    Create returnData.php

---
+++ returnData.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.php" media="screen">
+</head>
+<body>
+
+  Your Data: <br>
+  Your surename is:<?php echo $_POST["surename"]; ?><br>
+  Your name is: <?php echo $_POST["name"]; ?><br>
+  Your birthday is: <?php echo $_POST["bday"]; ?>
+
+</body>
+</html>
